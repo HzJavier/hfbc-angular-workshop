17cd1501b30dc15b304d4281aad07e78ec433980
HzJavier <jhernandez@ooyala.com>
2014-04-04 15:58:11 -0600

    Load data from server api

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,8 +1,10 @@
 var app = angular.module('workshopApp', []);
 
-app.controller('PlaceListCtrl', function ($scope) {
-  $scope.places = [
-    { 'name': 'Tec', 'location': '123.213, 123.242342' },
-    { 'name': 'IEEE', 'location': '123.33, 123.4334' }
-  ];
+app.controller('PlaceListCtrl', function ($scope, $http) {
+  $scope.places = [];
+
+  $http.get('/api/places').success(function (data) {
+    $scope.places = data.places;
+  });
+
 });
