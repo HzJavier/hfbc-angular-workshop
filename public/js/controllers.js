95779075e2264c57b350cbb05307a7dd45fe7b49
HzJavier <jhernandez@ooyala.com>
2014-04-04 22:58:09 -0600

    Add maps and markers

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -35,15 +35,49 @@ app.controller('PlaceListCtrl', ['$scope', '$http',
 
 app.controller('PlaceDetailCtrl', ['$scope', '$routeParams', '$http',
   function ($scope, $routeParams, $http) {
+    var domReady = false;
+    var map;
+    var latLng;
+
     $scope.place;
 
     $http.get('/api/places/' + $routeParams.placeId).success(function (data) {
       $scope.place = data;
+      if (domReady) {
+        loadMap();
+      }
     });
 
     $scope.getLocation = function () {
       alert('Location: ' + $scope.place.location);
     };
+
+    var mapDom = document.getElementById('map');
+    angular.element(mapDom).ready(function () {
+      domReady = true;
+      if ($scope.place) {
+        loadMap();
+      }
+    });
+
+    function loadMap() {
+      latLng = new google.maps.LatLng($scope.place.lat, $scope.place.lng);
+
+      map = new google.maps.Map(document.getElementById("map"), {
+        center: latLng,
+        zoom: 14 
+      }); 
+
+      addMarker(latLng, map);
+    }
+
+    function addMarker (latLng, map) {
+      var marker = new google.maps.Marker({
+        position: latLng,
+        map: map,
+        draggable: true
+      });
+    }
   }
 ]);
 
