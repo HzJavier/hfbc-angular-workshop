bd2e1942dea596c79bd7743caeb7f7eb52995d09
HzJavier <jhernandez@ooyala.com>
2014-04-04 19:54:26 -0600

    Add placeholder for map

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -46,3 +46,11 @@ app.controller('PlaceDetailCtrl', ['$scope', '$routeParams', '$http',
     };
   }
 ]);
+
+app.directive('holderFix', function () {
+  return {
+    link: function (scope, element, attrs) {
+      Holder.run({ images: element[0], nocss: true });
+    }
+  };
+});
